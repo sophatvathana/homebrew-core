cb27f19f6d67494f63853863c67bed164d411d7e
Sophat Vathana <sovathana.phat@gmail.com>
2019-07-23 09:41:44 +0700

    Update gdal.rb

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -1,7 +1,7 @@
 class Gdal < Formula
   desc "Geospatial Data Abstraction Library"
   homepage "https://www.gdal.org/"
-  url "https://download.osgeo.org/gdal/2.4.2/gdal-2.4.2.tar.xz"
+  url "https://download.osgeo.org/gdal/2.3.1/gdal-2.3.1.tar.xz"
   sha256 "dcc132e469c5eb76fa4aaff238d32e45a5d947dc5b6c801a123b70045b618e0c"
 
   bottle do
